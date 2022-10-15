b7ff70c30dc35707a022889a347c95f302bd0eff
gorzek-ryan <ryan.gorzek@gmail.com>
2022-10-14 23:22:10 -0700

    Rename folder to electro_viz.

---
+++ electro_viz/OptoStim.py
@@ -0,0 +1,13 @@
+# MIT License
+# Copyright (c) 2022 Ryan Gorzek
+# https://github.com/gorzek-ryan/electro_viz/blob/main/LICENSE
+# https://opensource.org/licenses/MIT
+
+class OptoStim:
+    '''
+    docstring
+    '''
+    
+    def __init__(self):
+        # ?
+        
---
+++ electro_viz/__init__.py
@@ -0,0 +1,4 @@
+# MIT License
+# Copyright (c) 2022 Ryan Gorzek
+# https://github.com/gorzek-ryan/electro_viz/blob/main/LICENSE
+# https://opensource.org/licenses/MIT
