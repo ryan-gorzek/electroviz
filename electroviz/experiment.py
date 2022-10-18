3520467d9207a228bd90e06480d02649b3846061
gorzek-ryan <ryan.gorzek@gmail.com>
2022-10-18 16:36:43 -0700

    Detect and add OptogeneticStimulus class instance to Experiment, rename source_data variable to nwb_file.

--- electroviz/experiment.py
+++ electroviz/experiment.py
@@ -3,18 +3,25 @@
 # https://github.com/gorzek-ryan/electroviz/blob/main/LICENSE
 # https://opensource.org/licenses/MIT
 
+from electroviz.population import Population
+from electroviz.optogeneticstimulus import OptogeneticStimulus
+
 class Experiment:
     '''
     docstring
     '''
     
-    def __init__(self, source_data):
+    def __init__(self, nwb_file):
         print('Experiment')
-        from electroviz.population import Population
-        #
-        electrodes_df = source_data.electrodes.to_dataframe()
+        # Create Population instance
+        electrodes_df = nwb_file.electrodes.to_dataframe()
         # get units dataframe
-        units_df = source_data.units.to_dataframe()
+        units_df = nwb_file.units.to_dataframe()
         # create population
         self.Population = Population(units_df, electrodes_df)
+        
+        # Create Stimulus instance(s)
+        if 'optotagging' in list(nwb_file.processing.keys()):
+            optotagging = nwb_file.processing['optotagging'].data_interfaces['optogenetic_stimulation']
+            self.OptogeneticStimulus = OptogeneticStimulus(optotagging)
         
