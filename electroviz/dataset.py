9ccabbf085a6b196c850caead67eca8eec27b422
gorzek-ryan <ryan.gorzek@gmail.com>
2022-10-18 16:35:27 -0700

    Rename source_data_path variable to nwb_file_path.

--- electroviz/dataset.py
+++ electroviz/dataset.py
@@ -8,16 +8,16 @@ class Dataset:
     docstring
     '''
     
-    def __init__(self, SourceDataPath):
+    def __init__(self, nwb_file_path):
         print('Dataset')
         from electroviz.experiment import Experiment
         import os
         from pynwb import NWBHDF5IO
-        if os.path.isfile(SourceDataPath) == True:
-            NWBIO = NWBHDF5IO(SourceDataPath, mode='r', load_namespaces=True)
-            NWBFile = NWBIO.read()
+        if os.path.isfile(nwb_file_path) == True:
+            nwb_io = NWBHDF5IO(nwb_file_path, mode='r', load_namespaces=True)
+            nwb_file = nwb_io.read()
             # add metadata
-            self.Experiment = Experiment(NWBFile)
+            self.Experiment = Experiment(nwb_file)
         else:
             raise Exception('Cannot load the specified file.')
         
