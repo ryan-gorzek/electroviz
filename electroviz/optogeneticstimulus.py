f6259b7754d81f32c95ec7d1c5637fc96563693a
gorzek-ryan <ryan.gorzek@gmail.com>
2022-10-18 22:08:34 -0700

    Add OptogeneticStimulus-specific features to Stimulus info_df.

--- electroviz/optogeneticstimulus.py
+++ electroviz/optogeneticstimulus.py
@@ -4,12 +4,17 @@
 # https://opensource.org/licenses/MIT
 
 from electroviz.stimulus import Stimulus
+import numpy as np
 
 class OptogeneticStimulus(Stimulus):
     '''
     docstring
     '''
     
-    def __init__(self, time_intervals_object):
+    def __init__(self, time_intervals_obj):
         print('OptogeneticStimulus')
-        super().__init__(time_intervals_object)
+        super().__init__(time_intervals_obj)
+        self.info_df.insert(1, "condition", list(time_intervals_obj['condition'].data))
+        self.info_df["duration"] = np.array(time_intervals_obj['duration'].data)
+        self.info_df["level"] = np.array(time_intervals_obj['duration'].data)
+        
