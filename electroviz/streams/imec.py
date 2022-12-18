ed26cc9219863efe73fd48ae5b4b0954bac00df7
gorzek-ryan <ryan.gorzek@gmail.com>
2022-12-18 14:35:00 -0800

    Read Kilosort output and create spike times sparse matrix in ImecSpikes subclass

--- electroviz/streams/imec.py
+++ electroviz/streams/imec.py
@@ -4,6 +4,7 @@
 # https://opensource.org/licenses/MIT
 
 import numpy as np
+from scipy import sparse
 
 class Imec:
     """
@@ -20,6 +21,9 @@ class Imec:
         # Get some basic data and parameters for easy access.
         self.imec_metadata = imec_metadata
         self.imec_binary = imec_binary
+        self.sampling_rate = float(self.imec_metadata["imSampRate"])
+        self.total_time = float(self.imec_metadata["fileTimeSecs"])
+        self.total_samples = int(self.sampling_rate * self.total_time)
 
 
 
@@ -55,12 +59,28 @@ class ImecSpikes(Imec):
                          imec_binary, 
                          kilosort_array)
 
-        print(kilosort_array.shape)
+        # Get Kilosort data from columns of kilosort_array.
+        (spike_clusters, spike_times) = np.hsplit(kilosort_array.flatten(), 2)
+        # Get the total number of units identified by Kilosort (index starts at 0).
+        self.total_units = int(np.max(spike_clusters) + 1)
+        # Build (sparse) spike times matrix.
+        self.spike_times = self._build_spike_times_matrix(spike_clusters, spike_times)
 
-    # def _build_times_matrix(
-    #         self, 
-    #         spike_times, 
-    #     ):
+    def _build_spike_times_matrix(
+            self, 
+            spike_clusters, 
+            spike_times, 
+        ):
+        """"""
+
+        # Create a units-by-samples scipy sparse coordinate matrix to store spike times.
+        full_shape = (self.total_units, self.total_samples)
+        row_idx = spike_clusters
+        col_idx = spike_times
+        data = np.ones((spike_times.size,))
+        spike_times_matrix = sparse.coo_matrix((data, (row_idx, col_idx)), shape=full_shape)
+        return spike_times_matrix
+
 
 # class ImecSync(Imec):
 #     """
