a11274a3cca938af1d2874bfd2d7802fd0272ec0
gorzek-ryan <ryan.gorzek@gmail.com>
2023-01-04 16:37:51 -0800

    Add population response plot, plot sparse noise kernels as heatmap

--- electroviz/viz/kernels.py
+++ electroviz/viz/kernels.py
@@ -7,10 +7,36 @@ import numpy as np
 from matplotlib import use as mpl_use
 import matplotlib.pyplot as plt
 
+def plot_population_response(
+        population, 
+        stimulus, 
+        time_window=[-0.050, 0.200], 
+    ):
+    """"""
+
+    sample_window = np.array(time_window)*30000
+    num_samples = int(sample_window[1] - sample_window[0])
+    responses = np.zeros((int(num_samples/30), len(stimulus)))
+    for event in stimulus:
+        window = (sample_window + event.sample_onset).astype(int)
+        resp = population.spike_times[:, window[0]:window[1]].sum(axis=0).squeeze()
+        responses[:, event.index] = np.sum(resp.reshape(250, -1), axis=1).squeeze()
+
+    mpl_use("Qt5Agg")
+    fig, axs = plt.subplots()
+    mean_response = np.nanmean(responses.squeeze(), axis=1)
+    axs.axvline(50, color="k", linestyle="dashed")
+    axs.plot(mean_response, color="b")
+    axs.set_xlabel("Time from onset (ms)")
+    axs.set_xticks([0, 50, 100, 150, 200, 250])
+    axs.set_xticklabels([-50, 0, 50, 100, 150, 200])
+    plt.show()
+
+
 def plot_sparse_noise_response(
         unit, 
         stimulus, 
-        time_window=[-0.02, 0.100], 
+        time_window=[-0.050, 0.200], 
     ):
     """"""
 
@@ -21,18 +47,19 @@ def plot_sparse_noise_response(
         window = (sample_window + event.sample_onset).astype(int)
         resp = unit.get_spike_times(window)
         x_idx, y_idx, c_idx = stimulus.get_params_index((event.posx, event.posy, event.contrast))
-        responses[:, x_idx, y_idx, c_idx, int(event.itrial)] = np.sum(resp.reshape(120, -1), axis=1)
+        responses[:, x_idx, y_idx, c_idx, int(event.itrial)] = np.sum(resp.reshape(250, -1), axis=1)
     
     mpl_use("Qt5Agg")
-    fig, axs = plt.subplots(10, 14)
+    fig, axs = plt.subplots(2, 1)
+    kernels = np.zeros((14, 10, 2))
     for posx, posy, contrast in stimulus.unique:
         x_idx, y_idx, c_idx = stimulus.get_params_index((posx, posy, contrast))
-        mean_response = np.nanmean(responses[:, x_idx, y_idx, c_idx, :].squeeze(), axis=1)
-        if c_idx == 0:
-            axs[y_idx, x_idx].plot(mean_response, color="b")
-        else:
-            axs[y_idx, x_idx].plot(mean_response, color="r")
-        # axs[y_idx, x_idx].axis("off")
-        axs[y_idx, x_idx].axvline(20, color="k")
+        response_sum = np.nansum(responses[20:100, x_idx, y_idx, c_idx, :].squeeze(), axis=(0, 1))
+        kernels[x_idx, y_idx, c_idx] += response_sum
+    axs[0].imshow(kernels[:, :, 0].T, cmap="hot")
+    axs[0].axis("off")
+    axs[1].imshow(kernels[:, :, 1].T, cmap="hot")
+    axs[1].axis("off")
     plt.show()
+    return fig
 
