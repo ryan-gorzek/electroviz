14b16d62c6257717cd2c2860e59c195b42b14332
ryan-gorzek <ryan.gorzek@gmail.com>
2023-01-06 14:08:44 -0800

    Get cluster_quality from Kilosort factory

--- electroviz/streams/spikes.py
+++ electroviz/streams/spikes.py
@@ -14,9 +14,10 @@ class Spikes:
 
     def __init__(
             self, 
+            total_imec_samples, 
             spike_clusters, 
             spike_times, 
-            total_imec_samples, 
+            cluster_quality, 
         ):
         """"""
 
@@ -24,8 +25,10 @@ class Spikes:
         self.total_units = int(np.max(spike_clusters) + 1)
         # Get the total number of samples in recording, passed from Imec data.
         self.total_samples = total_imec_samples
-        # Build (sparse) spike times matrix in compressed sparse row format.
+        # Build (sparse) spike times matrix in compressed sparse column format.
         self.times = self._build_spike_times_matrix(spike_clusters, spike_times)
+        # Store Kilosort quality labels.
+        self.cluster_quality = cluster_quality
 
     def _build_spike_times_matrix(
             self, 
@@ -39,16 +42,17 @@ class Spikes:
         row_idx = spike_clusters
         col_idx = spike_times
         data = np.ones((spike_times.size,))
-        spike_times_matrix = sparse.csr_matrix((data, (row_idx, col_idx)), shape=full_shape)
+        spike_times_matrix = sparse.csc_matrix((data, (row_idx, col_idx)), shape=full_shape, dtype=bool)
         return spike_times_matrix
 
     def drop_and_rebuild(
             self, 
-            drop_samples,
+            drop_samples, 
         ):
         """"""
 
         # Create column (sample) logical index from sample indices.
         col_mask = np.ones(self.total_samples, dtype=bool)
         col_mask[drop_samples] = False
-        return self.times[:, col_mask].tocoo()
+        self.total_samples += -len(drop_samples)
+        return self.times[:, col_mask]
