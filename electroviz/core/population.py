c202df5b11abc7be5771e145534dd2fd441cc13f
ryan-gorzek <ryan.gorzek@gmail.com>
2023-01-06 14:11:02 -0800

    Handle spike times matrix as csc in all cases, improve slicing efficiency in get_aligned_response, add slicing and array indexing to Population __getitem__ method, returning a subsetted Population object

--- electroviz/core/population.py
+++ electroviz/core/population.py
@@ -3,10 +3,14 @@
 # https://github.com/gorzek-ryan/electroviz/blob/main/LICENSE
 # https://opensource.org/licenses/MIT
 
+import copy
 import numpy as np
 import pandas as pd
 from scipy import sparse
 from electroviz.core.unit import Unit
+from matplotlib import use as mpl_use
+import matplotlib.pyplot as plt
+from scipy.stats import zscore
 
 class Population:
     """
@@ -20,36 +24,45 @@ class Population:
         ):
         """"""
 
-        self.sync = imec[0]
-        self.spikes = kilosort[0]
-        self.total_samples = self.spikes.total_samples
-        self.total_units = self.spikes.total_units
-        self.metrics = pd.DataFrame()
-        self.spike_times = sparse.csc_matrix(self.spikes.times)
-        max_spikes = np.max(self.spike_times.sum(axis=1))
+        self._Sync = imec[0]
+        self._Spikes = kilosort[0]
+        self.total_samples = self._Spikes.total_samples
+        self.total_units = self._Spikes.total_units
+        self.spike_times = self._Spikes.times
         # Create Unit objects.
         self._Units = []
         for uid in range(self.total_units):
-            unit = Unit(uid, self.sync, self.spikes)
+            unit = Unit(uid, self._Sync, self._Spikes)
             self._Units.append(unit)
+        # Populate unit metrics dataframe.
+        self.units = pd.DataFrame()
+        self.units["unit_id"] = np.arange(0, self.total_units)
+        self.units["cluster_quality"] = self._Spikes.cluster_quality
+        # firing rate, peak channel number
         
         # Define current index for iteration.
-        self._current_unit_idx = 0
+        self._current_Unit_idx = 0
 
     def plot_aligned_response(
             self, 
             stimulus, 
             time_window=[-0.050, 0.200], 
             bin_size=0.001, 
+            cmap="binary", 
         ):
         """"""
         
-        responses = self.get_aligned_response(self, stimulus, time_window, bin_size)
+        responses = self.get_aligned_response(stimulus, time_window, bin_size)
         mpl_use("Qt5Agg")
         fig, axs = plt.subplots()
         mean_response = np.nanmean(responses, axis=2)
-        z_response = zscore(mean_response.T, axis=1)
-        axs.imshow(z_response, cmap="binary")
+        z_response = zscore(mean_response, axis=1)
+        axs.imshow(z_response, cmap=cmap)
+        axs.set_xlabel("Time from onset (ms)")
+        axs.set_xticks([0, 50, 100, 150, 200, 250])
+        axs.set_xticklabels([-50, 0, 50, 100, 150, 200])
+        axs.set_ylabel("Unit")
+        fig.set_size_inches(5, 11)
         plt.show()
 
     def get_aligned_response(
@@ -59,75 +72,81 @@ class Population:
             bin_size=0.001, 
         ):
         """"""
-        #### Need to speed this up!
+        
         sample_window = np.array(time_window)*30000
         num_samples = int(sample_window[1] - sample_window[0])
         num_bins = int(num_samples/(bin_size*30000))
-        responses = np.zeros((num_bins, len(self), len(stimulus)))
+        responses = np.zeros((len(self), num_bins, len(stimulus)))
         for event in stimulus:
             window = (sample_window + event.sample_onset).astype(int)
-            resp = self.spike_times[:, window[0]:window[1]]
-            for unit_idx, unit_resp in enumerate(resp):
-                responses[:, unit_idx, event.index] = np.sum(unit_resp.reshape(num_bins, -1), axis=1).squeeze()
+            resp = self.spike_times[:, window[0]:window[1]].toarray()
+            bin_resp = resp.reshape((len(self), num_bins, -1)).sum(axis=2)
+            responses[:, :, event.index] = bin_resp
         return responses
 
-    def plot_averaged_response(
-            self, 
-            stimulus, 
-            time_window=[-0.050, 0.200], 
-            bin_size=0.001, 
-        ):
-        """"""
+    # def plot_averaged_response(
+    #         self, 
+    #         stimulus, 
+    #         time_window=[-0.050, 0.200], 
+    #         bin_size=0.001, 
+    #     ):
+    #     """"""
 
-        sample_window = np.array(time_window)*30000
-        num_samples = int(sample_window[1] - sample_window[0])
-        num_bins = int(num_samples/(bin_size*30000))
-        responses = np.zeros((num_bins, len(stimulus)))
-        for event in stimulus:
-            window = (sample_window + event.sample_onset).astype(int)
-            resp = self.spike_times[:, window[0]:window[1]].sum(axis=0).squeeze()
-            responses[:, event.index] = np.sum(resp.reshape(num_bins, -1), axis=1).squeeze()
-        mpl_use("Qt5Agg")
-        fig, axs = plt.subplots()
-        mean_response = np.nanmean(responses.squeeze(), axis=1)
-        axs.axvline(50, color="k", linestyle="dashed")
-        axs.plot(mean_response, color="b")
-        axs.set_xlabel("Time from onset (ms)")
-        axs.set_xticks([0, 50, 100, 150, 200, 250])
-        axs.set_xticklabels([-50, 0, 50, 100, 150, 200])
-        plt.show()
+    #     sample_window = np.array(time_window)*30000
+    #     num_samples = int(sample_window[1] - sample_window[0])
+    #     num_bins = int(num_samples/(bin_size*30000))
+    #     responses = np.zeros((num_bins, len(stimulus)))
+    #     for event in stimulus:
+    #         window = (sample_window + event.sample_onset).astype(int)
+    #         resp = self.spike_times[:, window[0]:window[1]].sum(axis=0).squeeze()
+    #         responses[:, event.index] = np.sum(resp.reshape(num_bins, -1), axis=1).squeeze()
+    #     mpl_use("Qt5Agg")
+    #     fig, axs = plt.subplots()
+    #     mean_response = np.nanmean(responses.squeeze(), axis=1)
+    #     axs.axvline(50, color="k", linestyle="dashed")
+    #     axs.plot(mean_response, color="b")
+    #     axs.set_xlabel("Time from onset (ms)")
+    #     axs.set_xticks([0, 50, 100, 150, 200, 250])
+    #     axs.set_xticklabels([-50, 0, 50, 100, 150, 200])
+    #     plt.show()
 
-    def sort(
-            self, 
-            metric_name, 
-        ):
-        """"""
+    # def sort(
+    #         self, 
+    #         metric_name, 
+    #     ):
+    #     """"""
 
-        if metric_name in self.metrics.columns:
-            sort_idx = np.argsort(self.metrics[metric_name])
-            return sort_idx
-            #### Need to reorder spike times matrix, _Units, metrics df
-            # self._Units
+    #     if metric_name in self.metrics.columns:
+    #         sort_idx = np.argsort(self.metrics[metric_name])
+    #         return sort_idx
+    #         #### Need to reorder spike times matrix, _Units, metrics df
+    #         # self._Units
 
 
-    def add_metric(
-            self, 
-            metric_name, 
-            metric_array, 
-        ):
-        """"""
+    # def add_metric(
+    #         self, 
+    #         metric_name, 
+    #         metric_array, 
+    #     ):
+    #     """"""
         
-        if len(metric_array) == len(self):
-            self.metrics[metric_name] = metric_array
+    #     if len(metric_array) == len(self):
+    #         self.metrics[metric_name] = metric_array
     
     def __getitem__(
             self, 
-            unit_idx, 
+            input, 
         ):
         """"""
 
-        unit = self._Units[unit_idx]
-        return unit
+        if isinstance(input, int):
+            subset = self._Units[unit_idx]
+        else:
+            try:
+                subset = self._get_subset(input)
+            except TypeError:
+                print("Failed.")
+        return subset
 
     def __iter__(self):
         return iter(self._Units)
@@ -140,4 +159,17 @@ class Population:
 
     def __len__(self):
         return len(self._Units)
+
+    def _get_subset(
+            self, 
+            slice_or_array, 
+        ):
+        """"""
+
+        subset = copy.copy(self)
+        subset._Units = self._Units[slice_or_array]
+        subset.spike_times = self.spike_times.tocsr()[slice_or_array, :].tocsc()
+        subset.total_units = len(subset._Units)
+        subset.units = self.units.iloc[slice_or_array]
+        return subset
         
