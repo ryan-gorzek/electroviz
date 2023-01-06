69d1f41d9ba88005f6274c6e466de11a194fcb97
ryan-gorzek <ryan.gorzek@gmail.com>
2023-01-06 10:44:04 -0800

    Add plotting methods for stimulus-aligned raster and PETH

--- electroviz/core/population.py
+++ electroviz/core/population.py
@@ -4,6 +4,7 @@
 # https://opensource.org/licenses/MIT
 
 import numpy as np
+import pandas as pd
 from scipy import sparse
 from electroviz.core.unit import Unit
 
@@ -23,6 +24,7 @@ class Population:
         self.spikes = kilosort[0]
         self.total_samples = self.spikes.total_samples
         self.total_units = self.spikes.total_units
+        self.metrics = pd.DataFrame()
         self.spike_times = sparse.csc_matrix(self.spikes.times)
         max_spikes = np.max(self.spike_times.sum(axis=1))
         # Create Unit objects.
@@ -33,6 +35,90 @@ class Population:
         
         # Define current index for iteration.
         self._current_unit_idx = 0
+
+    def plot_aligned_response(
+            self, 
+            stimulus, 
+            time_window=[-0.050, 0.200], 
+            bin_size=0.001, 
+        ):
+        """"""
+        
+        responses = self.get_aligned_response(self, stimulus, time_window, bin_size)
+        mpl_use("Qt5Agg")
+        fig, axs = plt.subplots()
+        mean_response = np.nanmean(responses, axis=2)
+        z_response = zscore(mean_response.T, axis=1)
+        axs.imshow(z_response, cmap="binary")
+        plt.show()
+
+    def get_aligned_response(
+            self, 
+            stimulus, 
+            time_window=[-0.050, 0.200], 
+            bin_size=0.001, 
+        ):
+        """"""
+        #### Need to speed this up!
+        sample_window = np.array(time_window)*30000
+        num_samples = int(sample_window[1] - sample_window[0])
+        num_bins = int(num_samples/(bin_size*30000))
+        responses = np.zeros((num_bins, len(self), len(stimulus)))
+        for event in stimulus:
+            window = (sample_window + event.sample_onset).astype(int)
+            resp = self.spike_times[:, window[0]:window[1]]
+            for unit_idx, unit_resp in enumerate(resp):
+                responses[:, unit_idx, event.index] = np.sum(unit_resp.reshape(num_bins, -1), axis=1).squeeze()
+        return responses
+
+    def plot_averaged_response(
+            self, 
+            stimulus, 
+            time_window=[-0.050, 0.200], 
+            bin_size=0.001, 
+        ):
+        """"""
+
+        sample_window = np.array(time_window)*30000
+        num_samples = int(sample_window[1] - sample_window[0])
+        num_bins = int(num_samples/(bin_size*30000))
+        responses = np.zeros((num_bins, len(stimulus)))
+        for event in stimulus:
+            window = (sample_window + event.sample_onset).astype(int)
+            resp = self.spike_times[:, window[0]:window[1]].sum(axis=0).squeeze()
+            responses[:, event.index] = np.sum(resp.reshape(num_bins, -1), axis=1).squeeze()
+        mpl_use("Qt5Agg")
+        fig, axs = plt.subplots()
+        mean_response = np.nanmean(responses.squeeze(), axis=1)
+        axs.axvline(50, color="k", linestyle="dashed")
+        axs.plot(mean_response, color="b")
+        axs.set_xlabel("Time from onset (ms)")
+        axs.set_xticks([0, 50, 100, 150, 200, 250])
+        axs.set_xticklabels([-50, 0, 50, 100, 150, 200])
+        plt.show()
+
+    def sort(
+            self, 
+            metric_name, 
+        ):
+        """"""
+
+        if metric_name in self.metrics.columns:
+            sort_idx = np.argsort(self.metrics[metric_name])
+            return sort_idx
+            #### Need to reorder spike times matrix, _Units, metrics df
+            # self._Units
+
+
+    def add_metric(
+            self, 
+            metric_name, 
+            metric_array, 
+        ):
+        """"""
+        
+        if len(metric_array) == len(self):
+            self.metrics[metric_name] = metric_array
     
     def __getitem__(
             self, 
@@ -47,7 +133,6 @@ class Population:
         return iter(self._Units)
 
     def __next__(self):
-        """"""
         if self._current_unit_idx < self.total_units:
             unit = self._Units[self._current_unit_idx]
             self._current_unit_idx += 1
