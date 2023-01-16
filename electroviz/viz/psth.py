8ac6b9935b2caf3c98332b3254b82b15acd42cfb
gorzek-ryan <ryan.gorzek@gmail.com>
2023-01-15 18:52:17 -0800

    Simplify to 1D response for now

--- electroviz/viz/psth.py
+++ electroviz/viz/psth.py
@@ -22,27 +22,23 @@ class PSTH:
             self, 
             time_window, 
             responses, 
+            decor=True, 
+            ax_in=None, 
         ):
         """"""
 
-        colors = ((0.0, 0.0, 0.0), (0.9, 0.2, 0.2))
         mpl_use("Qt5Agg")
         fig, ax = plt.subplots()
-        if responses.ndim == 2:
-            for resp, col in zip(responses, colors[:responses.ndim]):
-                t = np.linspace(0, resp.size, resp.size)
-                ax.bar(t, resp, color=col)
-        else:
-            t = np.linspace(0, responses.size, responses.size)
-            ax.bar(t, responses, color="k")
+        t = np.linspace(0, responses.size, responses.size)
+        ax.bar(t, responses, color="k")
         ax.set_xticks(np.linspace(0, t.size, 6))
         ax.set_xticklabels(np.linspace(*time_window, 6))
         ax.set_xlabel("Time from Onset (ms)", fontsize=16)
+        ax.set_ylabel("Spikes / second", fontsize=16)
         ends = np.array((responses.min(), responses.max()))
         rng = np.diff(ends)
         lims = ends + 0.1 * np.array((-rng, rng)).T
         ax.set_ylim(lims[0])
-        ax.set_ylabel("Spikes / second", fontsize=16)
         plt.show(block=False)
         plt.tight_layout()
         fig.set_size_inches(6, 6)
