59556891278e8c630b5fe17bd680232b5faf707d
gorzek-ryan <ryan.gorzek@gmail.com>
2023-01-21 16:58:06 -0800

    Update formatting and license years

--- electroviz/streams/__init__.py
+++ electroviz/streams/__init__.py
@@ -1,4 +1,5 @@
+
 # MIT License
-# Copyright (c) 2022 Ryan Gorzek
+# Copyright (c) 2022-3 Ryan Gorzek
 # https://github.com/gorzek-ryan/electroviz/blob/main/LICENSE
 # https://opensource.org/licenses/MIT
