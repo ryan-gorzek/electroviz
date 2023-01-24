a350ec8b58b736eb0b46fd3b8b3bb380396dd13d
ryan-gorzek <ryan.gorzek@gmail.com>
2023-01-23 20:11:38 -0800

    Add function for computing cross-correlation across full recording

--- electroviz/utils/cross_corr.py
+++ electroviz/utils/cross_corr.py
@@ -5,9 +5,26 @@
 # https://opensource.org/licenses/MIT
 
 import numpy as np
-from scipy.signal import correlate
+from scipy.signal import correlate, correlation_lags, butter, lfilter
 
 def cross_corr(
+        units, 
+        time_window=(-50, 50), 
+        bin_size=1, 
+    ):
+    """"""
+
+    spikes_0 = units[0].bin_spikes(bin_size=bin_size, type="count")
+    spikes_1 = units[1].bin_spikes(bin_size=bin_size, type="count")
+    xcorr = correlate(spikes_0, spikes_1, mode="same", method="fft")
+    lags = correlation_lags(spikes_0.size, spikes_1.size, mode="same")
+    window = np.logical_and(lags >= time_window[0]/bin_size, lags <= time_window[1]/bin_size)
+    b, a = butter(1, (75.0, 499.99), fs=1000/bin_size, btype="bandpass")
+    xcorr_filt = lfilter(b, a, xcorr)
+    return xcorr[window], xcorr_filt[window]
+
+
+def cross_corr_stim(
         self_unit, 
         other_unit, 
         stimulus, 
