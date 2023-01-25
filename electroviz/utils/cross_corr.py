6cdf61c702509fcd9f81ecf83ecf598525ee392f
ryan-gorzek <ryan.gorzek@gmail.com>
2023-01-25 09:45:44 -0800

    Add plot_cross_corr function

--- electroviz/utils/cross_corr.py
+++ electroviz/utils/cross_corr.py
@@ -6,11 +6,13 @@
 
 import numpy as np
 from scipy.signal import correlate, correlation_lags, butter, lfilter
+import matplotlib.pyplot as plt
+from matplotlib import use as mpl_use
 
 def cross_corr(
         units, 
         time_window=(-50, 50), 
-        bin_size=0.5, 
+        bin_size=1, 
     ):
     """"""
 
@@ -19,9 +21,35 @@ def cross_corr(
     xcorr = correlate(spikes_0, spikes_1, mode="same", method="fft")
     lags = correlation_lags(spikes_0.size, spikes_1.size, mode="same")
     window = np.logical_and(lags >= time_window[0]/bin_size, lags <= time_window[1]/bin_size)
-    b, a = butter(1, (75.0, 750), fs=1000/bin_size, btype="bandpass")
+    b, a = butter(1, (75.0, 499.99), fs=1000/bin_size, btype="bandpass")
     xcorr_filt = lfilter(b, a, xcorr)
     return xcorr[window], xcorr_filt[window]
+
+
+def plot_cross_corr(
+        units, 
+        save_path="", 
+    ):
+    """"""
+
+    xcorr_raw, xcorr_filt = cross_corr(units)
+    mpl_use("Qt5Agg")
+    fig, axs = plt.subplots(2, 1)
+    axs[0].bar(range(-50, 51), xcorr_raw, color=(0.9, 0.5, 0.5))
+    axs[0].set_xlabel("")
+    axs[0].set_xticklabels([])
+    axs[0].set_ylabel("Raw")
+    axs[0].set_title("Cross-Correlation")
+    axs[1].hlines(0, -50, 51, colors="k", linestyles="--")
+    axs[1].plot(range(-50, 51), xcorr_filt, color=(0.9, 0.5, 0.5))
+    axs[1].set_xlabel("Lag (ms)")
+    axs[1].set_ylabel("Filtered")
+    ylim = axs[1].get_ylim()
+    axs[1].fill([2, 2, 5, 5], ylim + ylim[::-1], color=(0.6, 0.6, 0.6, 0.5))
+    plt.show(block=False)
+    fig.set_size_inches(8, 16)
+    if save_path != "":
+        fig.savefig(save_path, bbox_inches="tight")
 
 
 def cross_corr_stim(
