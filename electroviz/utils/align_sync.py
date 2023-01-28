465d2ac41f109bebd8816a49e9d47496f144e4ae
ryan-gorzek <ryan.gorzek@gmail.com>
2023-01-27 19:50:44 -0800

    Rework LFP sync alignment

--- electroviz/utils/align_sync.py
+++ electroviz/utils/align_sync.py
@@ -72,7 +72,8 @@ def align_lf_sync(
     ):
     """"""
 
-    syncs = [nidaq[0], *imec_lf[::2]]
+    # Align the LFP syncs first.
+    syncs = [*imec_lf[::2]]
     # Get sample onsets, offsets, and durations from each object's sync signal.
     event_names = ["sample_onset", "sample_offset"]
     events = []
@@ -107,13 +108,42 @@ def align_lf_sync(
     rems_diff = rems - np.min(rems)
     for sync_idx, (tot, rem) in enumerate(zip(totals, rems_diff)):
         [drops[sync_idx].append(idx) for idx in range(int(tot - rem), tot) if rem != 0]
-
-    # Rebuild the NIDAQ signals, dropping the indices identified here.
-    nidaq_lf = copy.deepcopy(nidaq)
-    for obj in nidaq_lf:
-        obj.drop_and_rebuild(drops[0])
+    
     # Imec.
-    for obj, drop in zip(imec_lf, drops[1:]):
+    for obj, drop in zip(imec_lf, drops):
         obj.drop_and_rebuild(drop)
 
-    return nidaq_lf, imec_lf, drops
+    # Then, match the NIDAQ signals to the LFP syncs.
+    imec_events = [imec_lf[0].events[event_names].to_numpy().astype(int)]
+    nidaq_events, new_events = [], []
+    for ni in nidaq:
+        nidaq_events.append(ni.events[event_names].to_numpy().astype(int))
+        new_events.append(np.empty(ni.events[event_names].to_numpy().astype(int).shape))
+    pc_sets = nidaq_events[1].flatten()
+    photo_sets = nidaq_events[2].flatten()
+    for event_idx, ons_offs in enumerate(zip(imec_events[0], nidaq_events[0])):
+        imec_onoff = ons_offs[0]
+        sync_on, sync_off = ons_offs[1]
+        # Set NIDAQ sync events to Imec sync events.
+        new_events[0][event_idx, :] = imec_onoff
+        # Map PC Clock to LFP sync.
+        pc_log = np.logical_and(pc_sets >= sync_on, pc_sets <= sync_off)
+        if any(pc_log):
+            pc_in = pc_sets[pc_log]
+            for pc_set in pc_in:
+                pc_loc = int(((pc_set - sync_on) / (sync_off - sync_on)) * np.diff(imec_onoff)[0])
+                row, col = np.where(nidaq_events[1] == pc_set)
+                new_events[1][row, col] = int(pc_loc + imec_onoff[0])
+        # Map Photodiode to LFP sync.
+        photo_log = np.logical_and(photo_sets >= sync_on, photo_sets <= sync_off)
+        if any(photo_log):
+            photo_in = photo_sets[photo_log]
+            for photo_set in photo_in:
+                photo_loc = int(((photo_set - sync_on) / (sync_off - sync_on)) * np.diff(imec_onoff)[0])
+                row, col = np.where(nidaq_events[2] == photo_set)
+                new_events[2][row, col] = int(photo_loc + imec_onoff[0])
+    nidaq_lf = copy.deepcopy(nidaq)
+    for ni_lf, new_ev in zip(nidaq_lf, new_events):
+        ni_lf.events[event_names] = new_ev.astype(int)
+
+    return nidaq_lf, imec_lf
