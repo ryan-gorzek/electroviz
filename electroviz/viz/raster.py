c49069d5ca1bf0f3984185e83da2a0c70e770c6d
ryan-gorzek <ryan.gorzek@gmail.com>
2023-01-28 17:52:36 -0800

    Specify colormap

--- electroviz/viz/raster.py
+++ electroviz/viz/raster.py
@@ -26,6 +26,8 @@ class Raster:
             responses, 
             z_score=True, 
             ylabel="", 
+            yscale=None, 
+            cmap="binary", 
             fig_size=(6, 9), 
             save_path="", 
             ax_in=None, 
@@ -41,11 +43,16 @@ class Raster:
             aspect_ratio = "auto"
         if z_score == True:
             responses = zscore(responses, axis=1)
-        ax.imshow(responses, cmap="binary", aspect=aspect_ratio)
+        ax.imshow(responses, cmap=cmap, aspect=aspect_ratio)
         ax.set_xticks(np.linspace(0, responses.shape[1], 6))
         ax.set_xticklabels(np.linspace(*time_window, 6))
         ax.set_xlabel("Time from Onset (ms)", fontsize=16)
+        ax.set_yticks(np.arange(0, responses.shape[0] - 1, 20))
+        ax.set_yticklabels(np.arange(0, responses.shape[0] - 1, 20, dtype=int))
         ax.set_ylabel(ylabel, fontsize=16)
+        if yscale is not None:
+            yticks = ax.get_yticks()
+            ax.set_yticklabels(yscale[yticks])
         if ax_in is None:
             plt.show(block=False)
             plt.tight_layout()
